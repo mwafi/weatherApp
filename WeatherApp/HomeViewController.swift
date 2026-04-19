b1e1ef7aa74daee03beedf808bcac8bad7ac0719
Salma Hassanein <113758995+salmmmma@users.noreply.github.com>
2026-04-19 07:46:20 +0200

    UI: fix home screen UI programming and layout issues

--- WeatherApp/HomeViewController.swift
+++ WeatherApp/HomeViewController.swift
@@ -10,4 +10,20 @@ import UIKit
 class HomeViewController:UIViewController
 {
     
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
+    @IBOutlet weak var forecastButton: UIButton!
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var cityButton: UIButton!
+    @IBOutlet weak var weatherCardView: UIView!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var windValueLabel: UILabel!
+    @IBOutlet weak var humidityValueLabel: UILabel!
+    @IBOutlet weak var conditionLabel: UILabel!
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
+        }
 }

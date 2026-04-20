7910c2203609a489a82923b160ef468d87f59fd8
Salma Hassanein <113758995+salmmmma@users.noreply.github.com>
2026-04-20 19:04:19 +0200

    feat: add notification item UI component

---
+++ WeatherApp/NotificationScreen/NotificationsOverlayView.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationsOverlayView.swift
+//  WeatherApp
+//
+//  Created by Mohammed Hassanien on 20/04/2026.
+//
+
+import SwiftUI
+
+struct NotificationsOverlayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationsOverlayView()
+}
---
+++ WeatherApp/NotificationScreen/NotificationsScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  WeatherApp
+//
+//  Created by Mohammed Hassanien on 20/04/2026.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}

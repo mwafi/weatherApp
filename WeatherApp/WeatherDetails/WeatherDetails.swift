3e5ab717a8ab6b867cc69c01fc1caaee7a04c4f7
Salma Hassanein <113758995+salmmmma@users.noreply.github.com>
2026-04-22 11:28:02 +0200

    finish weather details screen

--- WeatherApp/WeatherDetails/WeatherDetails.swift
+++ WeatherApp/WeatherDetails/WeatherDetails.swift
@@ -9,7 +9,34 @@ import SwiftUI
 
 struct WeatherDetails: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            HomeBackgroundView()
+            
+            VStack(spacing:18) {
+                
+                TopBarView()
+                    .padding(.horizontal, 30)
+                
+                HStack {
+                    Text("Today")
+                        .font(.system(size: 28, weight: .bold))
+                    
+                    Spacer()
+                    
+                    Text("Sep, 12")
+                        .font(.system(size: 16))
+                }
+                .foregroundColor(.white)
+                .padding(.horizontal, 37)
+                
+                HourlyCardView()
+                
+                DailyForecastSectionView()
+                
+                Spacer()
+            }
+            .padding(.top)
+        }
     }
 }
 

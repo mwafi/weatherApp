232d4039075e4248bbf2440192e09567b8142e68
Salma Hassanein <113758995+salmmmma@users.noreply.github.com>
2026-04-22 14:29:11 +0200

    wrap home screen in navigation stack

--- WeatherApp/Home/HomeScreen.swift
+++ WeatherApp/Home/HomeScreen.swift
@@ -9,35 +9,41 @@ import SwiftUI
 
 struct HomeScreen: View {
     @State private var showNotifications = false
+    
     var body: some View {
-        ZStack {
-            HomeBackgroundView()
-            
-            VStack(spacing: 0) {
-                HomeHeaderView(cityName: "Semarang", showNotifications: $showNotifications)
-                                    .padding(.top, 18)
+        NavigationStack {
+            ZStack {
+                HomeBackgroundView()
                 
-                Spacer()
-                    .frame(height: 55)
+                VStack(spacing: 0) {
+                    HomeHeaderView(cityName: "Semarang", showNotifications: $showNotifications)
+                        .padding(.top, 18)
+                    
+                    Spacer()
+                        .frame(height: 55)
+                    
+                    WeatherIllustrationView(conditionName: "Cloudy")
+                    
+                    Spacer()
+                        .frame(height: 30)
+                    
+                    CurrentWeatherCardView()
+                    
+                    Spacer()
+                        .frame(height: 100)
+                    
+                    ForecastReportButtonView()
+                    
+                    Spacer()
+                }
                 
-                WeatherIllustrationView(conditionName: "Cloudy")
-                
-                Spacer()
-                    .frame(height: 30)
-                
-                CurrentWeatherCardView()
-                
-                Spacer()
-                    .frame(height: 100)
-                
-                ForecastReportButtonView()
-                
-                Spacer()
+                if showNotifications {
+                    NotificationsOverlayView(isPresented: $showNotifications)
+                        .transition(.move(edge: .bottom))
+                        .zIndex(1)
+                }
             }
-            if showNotifications {
-                            NotificationsOverlayView(isPresented: $showNotifications)
-                                .transition(.move(edge: .bottom))                         .zIndex(1)
-                        }
+            .navigationBarHidden(true)
         }
     }
 }

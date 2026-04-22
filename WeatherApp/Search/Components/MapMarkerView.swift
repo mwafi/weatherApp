2220cd5bd65371be35a529b36896bfda3c13900e
Salma Hassanein <113758995+salmmmma@users.noreply.github.com>
2026-04-22 04:21:21 +0200

    create custom map pin with the red marker and triangle thing

--- WeatherApp/Search/Components/MapMarkerView.swift
+++ WeatherApp/Search/Components/MapMarkerView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct MapMarkerView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            Image(systemName: "mappin.circle.fill")
+                .font(.system(size: 36))
+                .foregroundColor(.red)
+                .background(
+                    Circle()
+                        .fill(.white)
+                        .frame(width: 20, height: 20)
+                )
+                .shadow(radius: 4)
+
+            Image(systemName: "triangle.fill")
+                .resizable()
+                .frame(width: 10, height: 6)
+                .rotationEffect(.degrees(180))
+                .foregroundColor(.red)
+                .offset(y: -4)
+        }
+        .offset(y: -18)
     }
 }
 

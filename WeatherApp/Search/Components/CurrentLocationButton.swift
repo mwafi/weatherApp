e824e4a8427eeb3ca5e100f9345d9c86669f0357
Salma Hassanein <113758995+salmmmma@users.noreply.github.com>
2026-04-22 04:21:13 +0200

    fix location button was just placeholder text now it has icon and action

--- WeatherApp/Search/Components/CurrentLocationButton.swift
+++ WeatherApp/Search/Components/CurrentLocationButton.swift
@@ -4,15 +4,25 @@
 //
 //  Created by Mohammed Hassanien on 21/04/2026.
 //
-
 import SwiftUI
 
 struct CurrentLocationButton: View {
+    var action: () -> Void
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: action) {
+            Image(systemName: "scope")
+                .font(.system(size: 22, weight: .bold))
+                .foregroundColor(Color(red: 0.29, green: 0.35, blue: 0.53))
+                .frame(width: 56, height: 56)
+                .background(Color.white)
+                .clipShape(Circle())
+                .shadow(color: .black.opacity(0.15), radius: 10, x: 0, y: 5)
+        }
+        .buttonStyle(.plain)
     }
 }
 
 #Preview {
-    CurrentLocationButton()
+    CurrentLocationButton(action: {})
 }

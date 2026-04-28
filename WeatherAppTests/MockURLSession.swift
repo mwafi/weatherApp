db9353a358ee6a816954035cc81ad0db455d34c9
Salma Hassanein <113758995+salmmmma@users.noreply.github.com>
2026-04-28 12:20:06 +0300

    Add MockURLSession with configurable data and error

--- WeatherAppTests/MockURLSession.swift
+++ WeatherAppTests/MockURLSession.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+@testable import WeatherApp
+
+final class MockURLSession: URLSessionProtocol {
+
+    var data: Data?
+    var response: URLResponse?
+    var error: Error?
+
+    func data(from url: URL) async throws -> (Data, URLResponse) {
+        if let error {
+            throw error
+        }
+
+        return (
+            data ?? Data(),
+            response ?? HTTPURLResponse(
+                url: url,
+                statusCode: 200,
+                httpVersion: nil,
+                headerFields: nil
+            )!
+        )
+    }
+}

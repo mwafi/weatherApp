dfbbc71301beeaa289bc1ea23b1d51d25b0e32fc
Salma Hassanein <113758995+salmmmma@users.noreply.github.com>
2026-04-28 12:19:56 +0300

    Improve background contrast with white foreground

--- WeatherApp/Features/Home/View/Components/HomeBackgroundView.swift
+++ WeatherApp/Features/Home/View/Components/HomeBackgroundView.swift
@@ -26,17 +26,18 @@ struct HomeBackgroundView: View {
                 .resizable()
                 .scaledToFit()
                 .frame(width: 112, height: 235)
-                .opacity(0.3)
+                .foregroundColor(.white)
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
                 .offset(x: -25, y: 92)
+                .contrast(1.5)
             
             Image("Vector 11")
                 .resizable()
                 .scaledToFit()
                 .frame(width: 425, height: 366.5)
-                .opacity(0.3)
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
                 .offset(x: 59, y: -54)
+                .contrast(1.5)
             
             
         }
